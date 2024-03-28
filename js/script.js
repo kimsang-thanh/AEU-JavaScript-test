bba9606c9b1ae5bd9c984cc1b9fd99ca675189b0
david-tho <thodavid110@gmail.com>
2024-03-28 19:51:06 +0700

    add functionality

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-//
-// document.getElemetById
+const inputRotate = document.querySelector(".input-rotate");
+const inputScale = document.querySelector(".input-scale");
+const scaleLabel = document.querySelector(".scale-label");
+const rotateLabel = document.querySelector(".rotate-label");
+const card = document.querySelector(".card");
+
+function rotateCard(e) {
+  const { value } = e.target;
+
+  rotateLabel.textContent = `Rotate ${value}`;
+  card.style.transform = `rotate(${value}deg)`;
+}
+
+function scalecard(e) {
+  const { value } = e.target;
+
+  scaleLabel.textContent = `Scale ${value}`;
+  card.style.transform = `scale(${value})`;
+}
+
+inputRotate.addEventListener("input", rotateCard);
+
+inputScale.addEventListener("input", scalecard);
