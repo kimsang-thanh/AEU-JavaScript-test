52d2cf17375aecdfb4dad5237623163c976ca0ab
david-tho <thodavid110@gmail.com>
2024-03-29 20:20:25 +0700

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const inputRotate = document.querySelector(".input-rotate");
 const inputScale = document.querySelector(".input-scale");
 const scaleLabel = document.querySelector("#scale-label");
 const rotateLabel = document.querySelector("#rotate-label");
+const transformValue = document.querySelector(".transform-value");
 const card = document.querySelector(".card");
 const obj = {
   rotate: 0,
@@ -12,6 +13,9 @@ function rotateCard(e) {
   const { value } = e.target;
   obj.rotate = value;
 
+  transformValue.textContent = `scale(${obj.scale / 100}) rotate(${
+    obj.rotate
+  }deg);`;
   rotateLabel.textContent = `Rotate ${value}`;
   card.style.transform = `rotate(${value}deg) scale(${obj.scale / 100})`;
 }
@@ -23,9 +27,21 @@ function scalecard(e) {
 
   obj.scale = value;
 
+  transformValue.textContent = `scale(${obj.scale / 100}) rotate(${
+    obj.rotate
+  }deg);`;
   scaleLabel.textContent = `Scale ${calcValue}`;
   card.style.transform = `scale(${calcValue}) rotate(${obj.rotate}deg)`;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  inputRotate.value = obj.rotate;
+  inputScale.value = obj.scale;
+
+  transformValue.textContent = `scale(${obj.scale / 100}) rotate(${
+    obj.rotate
+  }deg);`;
+});
 
 inputRotate.addEventListener("input", rotateCard);
 
